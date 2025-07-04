3513947f37fb0e68a5936ea7bc317d5042e5c858
ReemHazza3 <reemhazzaamosa95@gmail.com>
2025-07-04 04:34:16 +0300

    Added Java Doc for City and Coordinates data classes

--- app/src/main/java/com/klivvr/assignment/data/City.kt
+++ app/src/main/java/com/klivvr/assignment/data/City.kt
@@ -2,6 +2,9 @@ package com.klivvr.assignment.data
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Represents the structure of a single city entry in the JSON file.
+ */
 data class City(
     @SerializedName("country")
     val country: String,
--- app/src/main/java/com/klivvr/assignment/data/Coordinates.kt
+++ app/src/main/java/com/klivvr/assignment/data/Coordinates.kt
@@ -2,6 +2,9 @@ package com.klivvr.assignment.data
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Represents the geographical coordinates.
+ */
 data class Coordinates(
     @SerializedName("lon")
     val lon: Double,
